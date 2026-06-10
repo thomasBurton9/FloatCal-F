8768048234bd1851c09a4d30ffbdbf3acff44727
Thomas Burton <thomasburton9@protonmail.com>
2026-06-10 15:41:42 +1000

    feat: connected expo go frontend to health backend

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const API_URL = 'http://192.168.68.108:8000/api/health'
+
 export default function App() {
+  const [result, setResult] = useState("Loading...")
+
+  async function getHealth() {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+
+      setResult(JSON.stringify(data));
+    } catch (error) {
+      setResult(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getHealth();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{result}</Text>
     </View>
   );
 }
