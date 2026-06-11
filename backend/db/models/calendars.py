b52518421950d634ac0dcb2948291f9b406ee7d7
Thomas Burton <thomasburton9@protonmail.com>
2026-06-11 18:56:59 +1000

    feat: added calendar member database model

--- backend/db/models/calendars.py
+++ backend/db/models/calendars.py
@@ -1,8 +1,8 @@
 """
-Contains table Calendars 
+Contains tables Calendars and Calendar Members
 """
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from db.base import Base
@@ -20,3 +20,18 @@ class Calendar(Base):
     created_by_user_id: Mapped[int] = mapped_column(
         ForeignKey("Users.user_id"), nullable=False
     )
+
+
+class CalendarMember(Base):
+    __tablename__ = "CalendarMembers"
+    __table_args__ = UniqueConstraint(
+        "calendar_id", "user_id", name="unique_calendar_user"
+    ) # Make sure that there are no duplicate memberships of a user in a calendar 
+
+    calendar_member_id: Mapped[int] = mapped_column(primary_key=True)
+
+    calendar_id: Mapped[int] = mapped_column(
+        ForeignKey("Calendars.calendar_id"), nullable=False
+    )
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("Users.user_id"), nullable=False)
