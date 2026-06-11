8f20670583ca59703e8b34ad32fee31cdbe45ae4
Thomas Burton <thomasburton9@protonmail.com>
2026-06-11 19:47:40 +1000

    feat: added floating task database model

--- backend/db/models/items.py
+++ backend/db/models/items.py
@@ -1,5 +1,5 @@
 """
-Contains table FixedEvent
+Contains tables Fixed Event and Floating Task
 """
 
 from sqlalchemy import ForeignKey
@@ -31,8 +31,40 @@ class FixedEvent(Base):
 
     notes: Mapped[str | None] = mapped_column(nullable=True)
 
-    recurrence_rule: Mapped[str] = (
-        mapped_column()
+    recurrence_rule: Mapped[str | None] = mapped_column(
+        nullable=True
     )  # Can be None, "Daily", "Weekly", "Fortnightly" etc - Maybe make this a constraint
 
     reminder: Mapped[bool] = mapped_column(default=False)
+
+
+class FloatingTask(Base):
+    __tablename__ = "FloatingTasks"
+
+    task_id: Mapped[int] = mapped_column(primary_key=True)
+
+    calendar_id: Mapped[int] = mapped_column(
+        ForeignKey("Calendars.calendar_id"), nullable=False
+    )
+
+    name: Mapped[str] = mapped_column(nullable=False)
+
+    date: Mapped[dt.date] = mapped_column(nullable=False)
+
+    duration_minutes: Mapped[int] = mapped_column(nullable=False)
+
+    notes: Mapped[str | None] = mapped_column(nullable=True)
+
+    recurrence_rule: Mapped[str | None] = mapped_column(
+        nullable=True
+    )  # Can be None, "Daily", "Weekly", "Fortnightly" etc - Maybe make this a constraint directly in db
+
+    reminder: Mapped[bool] = mapped_column(default=False)
+
+    preferred_window: Mapped[str | None] = mapped_column(
+        nullable=True
+    )  # Potentially connect to Settings.scheduling_windows
+
+    scheduled_start: Mapped[dt.time | None] = mapped_column(nullable=True)
+
+    manually_scheduled: Mapped[bool] = mapped_column(default=False)
