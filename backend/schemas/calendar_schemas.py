e9110dd6342dc738fe302d881fc3461b400b5669
Thomas Burton <thomasburton9@protonmail.com>
2026-06-22 12:06:48 +1000

    fix: aligned length of calendar names to original documentation

--- backend/schemas/calendar_schemas.py
+++ backend/schemas/calendar_schemas.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class CreateCalendar(BaseModel):
-    name: Annotated[str, Field(min_length=1, max_length=63)]
+    name: Annotated[str, Field(min_length=1, max_length=16)]
     # 8 digit hexcode #RRGGBBAA
     # Potentially make the checks more strict
     colour: Annotated[str, Field(min_length=9, max_length=9)]
