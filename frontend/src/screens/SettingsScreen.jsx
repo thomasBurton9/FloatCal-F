985fa9e7470add19479e432e7e24ee4a79c74903
Thomas Burton <thomasburton9@protonmail.com>
2026-07-03 17:49:00 +1000

    feat(frontend): added displaying all settings data and settings page styling

--- frontend/src/screens/SettingsScreen.jsx
+++ frontend/src/screens/SettingsScreen.jsx
@@ -4,7 +4,6 @@ import { API_URL } from "../constants";
 
 export default function SettingsScreen({ userId, setUserId }) {
   const [settings, setSettings] = useState(null);
-  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     // Defining function inside useEffect to prevent cascading renders??
@@ -15,7 +14,6 @@ export default function SettingsScreen({ userId, setUserId }) {
       if (settingsData) {
         console.log("Settings", settingsData);
         setSettings(settingsData);
-        setLoaded(true);
       }
     }
     loadSettings();
@@ -30,13 +28,33 @@ export default function SettingsScreen({ userId, setUserId }) {
         <View style={style.settingsSection}>
           <Text>Scheduler</Text>
           <View style={style.individualSetting}>
-            <Text>
+            <Text style={style.individualSettingInfo}>
               Sleep Window:{" "}
-              {loaded
+              {settings
                 ? formatTime(settings["sleep_start"]) +
                   "-" +
                   formatTime(settings["sleep_end"])
-                : null}
+                : "Loading..."}
+            </Text>
+            <Pressable style={style.editSettingsButton}>
+              <Text>{">"}</Text>
+            </Pressable>
+          </View>
+          <View style={style.individualSetting}>
+            <Text style={style.individualSettingInfo}>
+              Buffer Time:{" "}
+              {settings ? settings["buffer_minutes"] + "min" : "Loading..."}
+            </Text>
+            <Pressable style={style.editSettingsButton}>
+              <Text>{">"}</Text>
+            </Pressable>
+          </View>
+          <View style={style.individualSetting}>
+            <Text style={style.individualSettingInfo}>
+              Preferred Times:{" "}
+              {settings
+                ? formatSchedulingWindows(settings["scheduling_windows"])
+                : "Loading..."}
             </Text>
             <Pressable style={style.editSettingsButton}>
               <Text>{">"}</Text>
@@ -45,6 +63,27 @@ export default function SettingsScreen({ userId, setUserId }) {
         </View>
         <View style={style.settingsSection}>
           <Text>Notifications</Text>
+          {/* Use react native - Switch for this */}
+          <View style={style.individualSetting}>
+            <Text style={style.individualSettingInfo}>
+              Notifications:{" "}
+              {settings
+                ? String(settings["notifications_enabled"])
+                : "Loading..."}
+            </Text>
+
+            <Pressable style={style.editSettingsButton}>
+              <Text>{">"}</Text>
+            </Pressable>
+          </View>
+          <View style={style.individualSetting}>
+            <Text style={style.individualSettingInfo}>
+              Sound: {settings ? settings["notification_sound"] : "Loading..."}
+            </Text>
+            <Pressable style={style.editSettingsButton}>
+              <Text>{">"}</Text>
+            </Pressable>
+          </View>
         </View>
         <View style={style.settingsSection}>
           <Text>Account</Text>
@@ -53,7 +92,6 @@ export default function SettingsScreen({ userId, setUserId }) {
           </Pressable>
         </View>
       </View>
-      <Text>{userId}</Text>
     </>
   );
 }
@@ -81,9 +119,20 @@ async function fetchSettings(userId) {
 
 // Format time returned from api into more readable time
 // i.e. 23:00:00 -> 23:000
-
 function formatTime(time) {
   return time.slice(0, 5);
+}
+
+function formatSchedulingWindows(scheduling_windows) {
+  if (!scheduling_windows) {
+    return "No windows configured";
+  }
+
+  // Inform user if they have multiple scheduling windows
+  if (Object.keys(scheduling_windows).length > 1) {
+    return `${Object.keys(scheduling_windows)[0]} ...`;
+  }
+  return Object.keys(scheduling_windows)[0];
 }
 const style = StyleSheet.create({
   screen: {
@@ -94,9 +143,18 @@ const style = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center",
   },
+  individualSettingInfo: {
+    paddingLeft: 10,
+  },
   individualSetting: {
+    justifyContent: "space-between", // Push button/arrow to the far right
     flexDirection: "row",
     alignItems: "center",
+    minWidth: "75%",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderRadius: 5,
+    marginVertical: 10,
   },
   editSettingsButton: {
     padding: 10,
