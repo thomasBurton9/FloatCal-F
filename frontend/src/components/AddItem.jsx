2d2a5882d0c217dc87d1f25e0d52bf4da0842ca5
Thomas Burton <thomasburton9@protonmail.com>
2026-07-19 18:53:31 +1000

    feat(frontend): added frontend modal to add items

--- frontend/src/components/AddItem.jsx
+++ frontend/src/components/AddItem.jsx
@@ -7,6 +7,7 @@ import {
   View,
   Switch,
   Alert,
+  Keyboard,
 } from "react-native";
 import { useEffect, useState } from "react";
 import DateTimePicker from "@react-native-community/datetimepicker";
@@ -14,6 +15,7 @@ import { Dropdown } from "react-native-element-dropdown";
 import { fetchCalendars } from "../api/calendarApi.js";
 import { fetchSettings } from "../api/settingsApi.js";
 import { createFixedEvent, createFloatingTask } from "../api/itemApi.js";
+import { SafeAreaView } from "react-native-safe-area-context"; // Modal does not respect the safearea view from App.jsx
 
 export default function AddItem({ isVisible, setCurrentModal, userId }) {
   const [itemType, setItemType] = useState("Floating");
@@ -74,196 +76,219 @@ export default function AddItem({ isVisible, setCurrentModal, userId }) {
   return (
     <>
       <Modal
-        style={styles.addItemModal}
+        transparent
         animationType="slide"
         allowSwipeDismissal={true}
         onRequestClose={() => null} // TODO: Implement function
         visible={isVisible} // currentModal === "addItem"
       >
-        <View style={styles.addItemModalContainer}>
-          <View style={styles.topBar}>
-            <Pressable onPress={() => setCurrentModal(null)}>
-              <Text>X</Text>
-            </Pressable>
-            <Text>Add Task</Text>
-          </View>
-          <ItemTypeSwitcher
-            itemType={itemType}
-            setItemType={setItemType}
-          ></ItemTypeSwitcher>
-          <View style={styles.addItemInputs}>
-            {/* Name */}
-            <View style={styles.individualInput}>
-              <Text>Name</Text>
-              <TextInput
-                value={itemFields.name}
-                maxLength={63}
-                style={styles.inputField}
-                onChangeText={(name) => setItemFields({ ...itemFields, name })}
-              ></TextInput>
-            </View>
-            {/* Date */}
-            <View style={styles.individualInput}>
-              <Text>Date</Text>
-              <DateTimePicker
-                value={itemFields.date}
-                mode="date"
-                onChange={(_, date) => {
-                  if (!date) {
-                    return;
-                  }
-                  setItemFields({ ...itemFields, date });
-                }}
-              ></DateTimePicker>
-            </View>
-            {/* Calendar */}
-            <View style={styles.individualInput}>
-              <Text>Calendar</Text>
-              <Dropdown
-                data={calendars}
-                labelField="name"
-                valueField="calendar_id"
-                value={itemFields.calendar}
-                placeholder="Select calendar"
-                style={styles.dropdown}
-                onChange={(calendar) =>
-                  setItemFields({
-                    ...itemFields,
-                    calendar: calendar.calendar_id,
-                  })
-                }
-              ></Dropdown>
-            </View>
-            {itemType === "Floating" ? (
-              <>
-                {/* Duration */}
+        <Pressable onPress={Keyboard.dismiss} style={{ flex: 1 }}>
+          {" "}
+          {/* Allow for dismissal of keyboard some fields */}
+          <SafeAreaView edges={["top"]} style={styles.addItemModal}>
+            <View style={styles.addItemModalContainer}>
+              <View style={styles.topBar}>
+                <Pressable onPress={() => setCurrentModal(null)}>
+                  <Text
+                    style={{
+                      minWidth: 30,
+                      minHeight: 30,
+                      justifyContent: "center",
+                      textAlign: "center",
+                      alignItems: "center",
+                      borderStyle: "solid",
+                      borderRadius: "100%",
+                      borderWidth: 2,
+                    }}
+                  >
+                    X
+                  </Text>
+                </Pressable>
+                <Text>Add Task</Text>
+              </View>
+              <ItemTypeSwitcher
+                itemType={itemType}
+                setItemType={setItemType}
+              ></ItemTypeSwitcher>
+              <View style={styles.addItemInputs}>
+                {/* Name */}
                 <View style={styles.individualInput}>
-                  <Text>Duration in minutes</Text>
+                  <Text>Name</Text>
                   <TextInput
-                    value={itemFields.duration}
-                    inputMode="numeric"
-                    placeholder="1-1440"
+                    value={itemFields.name}
+                    maxLength={63}
                     style={styles.inputField}
-                    onChangeText={(duration) =>
-                      setItemFields({ ...itemFields, duration })
+                    onChangeText={(name) =>
+                      setItemFields({ ...itemFields, name })
                     }
                   ></TextInput>
                 </View>
-                {/* Preferred window */}
+                {/* Date */}
                 <View style={styles.individualInput}>
-                  <Text>Preferred window</Text>
+                  <Text>Date</Text>
+                  <DateTimePicker
+                    value={itemFields.date}
+                    mode="date"
+                    onChange={(_, date) => {
+                      if (!date) {
+                        return;
+                      }
+                      setItemFields({ ...itemFields, date });
+                    }}
+                  ></DateTimePicker>
+                </View>
+                {/* Calendar */}
+                <View style={styles.individualInput}>
+                  <Text>Calendar</Text>
                   <Dropdown
-                    data={preferredWindowOptions}
+                    data={calendars}
                     labelField="name"
-                    valueField="name"
-                    value={itemFields.preferredWindow}
-                    placeholder="No preference"
+                    valueField="calendar_id"
+                    value={itemFields.calendar}
+                    placeholder="Select calendar"
                     style={styles.dropdown}
-                    onChange={(preferredWindow) =>
+                    onChange={(calendar) =>
                       setItemFields({
                         ...itemFields,
-                        preferredWindow: preferredWindow.name,
+                        calendar: calendar.calendar_id,
                       })
                     }
                   ></Dropdown>
                 </View>
-              </>
-            ) : (
-              <>
-                {/* Start time */}
+                {itemType === "Floating" ? (
+                  <>
+                    {/* Duration */}
+                    <View style={styles.individualInput}>
+                      <Text>Duration in minutes</Text>
+                      <TextInput
+                        value={itemFields.duration}
+                        inputMode="numeric"
+                        placeholder="1-1440"
+                        style={styles.inputField}
+                        onChangeText={(duration) =>
+                          setItemFields({ ...itemFields, duration })
+                        }
+                      ></TextInput>
+                    </View>
+                    {/* Preferred window */}
+                    <View style={styles.individualInput}>
+                      <Text>Preferred window</Text>
+                      <Dropdown
+                        data={preferredWindowOptions}
+                        labelField="name"
+                        valueField="name"
+                        value={itemFields.preferredWindow}
+                        placeholder="No preference"
+                        style={styles.dropdown}
+                        onChange={(preferredWindow) =>
+                          setItemFields({
+                            ...itemFields,
+                            preferredWindow: preferredWindow.name,
+                          })
+                        }
+                      ></Dropdown>
+                    </View>
+                  </>
+                ) : (
+                  <>
+                    {/* Start time */}
+                    <View style={styles.individualInput}>
+                      <Text>Start time</Text>
+                      <DateTimePicker
+                        value={itemFields.startTime}
+                        mode="time"
+                        is24Hour={true}
+                        onChange={(_, startTime) => {
+                          if (!startTime) {
+                            return;
+                          }
+                          setItemFields({ ...itemFields, startTime });
+                        }}
+                      ></DateTimePicker>
+                    </View>
+                    {/* End time */}
+                    <View style={styles.individualInput}>
+                      <Text>End time</Text>
+                      <DateTimePicker
+                        value={itemFields.endTime}
+                        mode="time"
+                        is24Hour={true}
+                        onChange={(_, endTime) => {
+                          if (!endTime) {
+                            return;
+                          }
+                          setItemFields({ ...itemFields, endTime });
+                        }}
+                      ></DateTimePicker>
+                    </View>
+                  </>
+                )}
+                {/* Notes */}
                 <View style={styles.individualInput}>
-                  <Text>Start time</Text>
-                  <DateTimePicker
-                    value={itemFields.startTime}
-                    mode="time"
-                    is24Hour={true}
-                    onChange={(_, startTime) => {
-                      if (!startTime) {
-                        return;
-                      }
-                      setItemFields({ ...itemFields, startTime });
-                    }}
-                  ></DateTimePicker>
+                  <Text>Notes</Text>
+                  <TextInput
+                    value={itemFields.notes}
+                    maxLength={319}
+                    multiline={true}
+                    style={styles.inputField}
+                    onChangeText={(notes) =>
+                      setItemFields({ ...itemFields, notes })
+                    }
+                  ></TextInput>
                 </View>
-                {/* End time */}
+                {/* Recurrence */}
                 <View style={styles.individualInput}>
-                  <Text>End time</Text>
-                  <DateTimePicker
-                    value={itemFields.endTime}
-                    mode="time"
-                    is24Hour={true}
-                    onChange={(_, endTime) => {
-                      if (!endTime) {
-                        return;
-                      }
-                      setItemFields({ ...itemFields, endTime });
-                    }}
-                  ></DateTimePicker>
+                  <Text>Recurrence</Text>
+                  <Switch
+                    value={itemFields.recurrenceOn}
+                    onValueChange={(recurrenceOn) =>
+                      setItemFields({ ...itemFields, recurrenceOn })
+                    }
+                  ></Switch>
                 </View>
-              </>
-            )}
-            {/* Notes */}
-            <View style={styles.individualInput}>
-              <Text>Notes</Text>
-              <TextInput
-                value={itemFields.notes}
-                maxLength={319}
-                multiline={true}
-                style={styles.inputField}
-                onChangeText={(notes) =>
-                  setItemFields({ ...itemFields, notes })
-                }
-              ></TextInput>
-            </View>
-            {/* Recurrence */}
-            <View style={styles.individualInput}>
-              <Text>Recurrence</Text>
-              <Switch
-                value={itemFields.recurrenceOn}
-                onValueChange={(recurrenceOn) =>
-                  setItemFields({ ...itemFields, recurrenceOn })
-                }
-              ></Switch>
-            </View>
-            {itemFields.recurrenceOn ? (
-              <View style={styles.individualInput}>
-                <Text>Recurrence rule</Text>
-                <TextInput
-                  value={itemFields.recurrenceRule}
-                  placeholder="daily, weekly, fortnightly, monthly or yearly"
-                  style={styles.inputField}
-                  onChangeText={(recurrenceRule) =>
-                    setItemFields({ ...itemFields, recurrenceRule })
+                {itemFields.recurrenceOn ? (
+                  <View style={styles.individualInput}>
+                    <Text>Recurrence rule</Text>
+                    <TextInput
+                      value={itemFields.recurrenceRule}
+                      placeholder="daily, weekly, fortnightly, monthly or yearly"
+                      style={styles.inputField}
+                      onChangeText={(recurrenceRule) =>
+                        setItemFields({ ...itemFields, recurrenceRule })
+                      }
+                    ></TextInput>
+                  </View>
+                ) : null}
+                {/* Reminder */}
+                <View style={styles.individualInput}>
+                  <Text>Reminder</Text>
+                  <Switch
+                    value={itemFields.remindersOn}
+                    onValueChange={(remindersOn) =>
+                      setItemFields({ ...itemFields, remindersOn })
+                    }
+                  ></Switch>
+                </View>
+                {/* Add Item Button*/}
+                <Pressable
+                  style={styles.addItemButton}
+                  onPress={() =>
+                    handleAddItem(
+                      itemType,
+                      setCurrentModal,
+                      itemFields,
+                      setItemFields,
+                    )
                   }
-                ></TextInput>
+                >
+                  <Text>
+                    {itemType === "Floating" ? "Add Task" : "Add Event"}
+                  </Text>
+                </Pressable>
               </View>
-            ) : null}
-            {/* Reminder */}
-            <View style={styles.individualInput}>
-              <Text>Reminder</Text>
-              <Switch
-                value={itemFields.remindersOn}
-                onValueChange={(remindersOn) =>
-                  setItemFields({ ...itemFields, remindersOn })
-                }
-              ></Switch>
             </View>
-            {/* Add Item Button*/}
-            <Pressable
-              style={styles.addItemButton}
-              onPress={() =>
-                handleAddItem(
-                  itemType,
-                  setCurrentModal,
-                  itemFields,
-                  setItemFields,
-                )
-              }
-            >
-              <Text>{itemType === "Floating" ? "Add Task" : "Add Event"}</Text>
-            </Pressable>
-          </View>
-        </View>
+          </SafeAreaView>
+        </Pressable>
       </Modal>
     </>
   );
@@ -422,8 +447,22 @@ function ItemTypeSwitcher({ itemType, setItemType }) {
 
 const styles = StyleSheet.create({
   addItemModal: {
+    flex: 1,
+    paddingTop: 50,
+    backgroundColor: "rgba(0, 0, 0, 0.12)", // If kept white it will just blend in with the modal
+  },
+  addItemModalContainer: {
+    width: "100%",
+    maxHeight: "95%",
     alignItems: "center", // Center content horizontally
-    justifyContent: "center", // Center content vertically
+    // justifyContent: "center", // Center content vertically
+    backgroundColor: "white",
+    flex: 1, // Force expand to full screen
+    borderStyle: "solid",
+    borderTopRightRadius: 12,
+    borderTopLeftRadius: 12,
+    borderWidth: 2,
+    padding: 16,
   },
   itemTypeSwitcher: {
     flexDirection: "row",
@@ -437,7 +476,9 @@ const styles = StyleSheet.create({
   nonCurrentType: {
     backgroundColor: "white",
   },
-
+  topBar: {
+    flexDirection: "row",
+  },
   itemType: {
     padding: 10,
     width: 100,
@@ -456,9 +497,17 @@ const styles = StyleSheet.create({
 
   addItemInputs: {
     flexDirection: "column",
+    gap: 6,
+    width: "100%",
   },
   individualInput: {
     flexDirection: "column",
+    gap: 4,
+  },
+  inputField: {
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderRadius: 6,
   },
   dropdown: {
     minHeight: 34,
