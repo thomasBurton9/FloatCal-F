e1ea487c562b2463c8205b200c2cbafece82a6c2
Thomas Burton <thomasburton9@protonmail.com>
2026-07-21 19:12:11 +1000

    chore(frontend): swapped date helper file from js to ts

---
+++ frontend/src/helpers/dateHelpers.ts
@@ -0,0 +1,56 @@
+// Formats date object into api receivable YYYY-MM-DD
+export function formatDate(date: Date | string): string {
+  if (!date) {
+    console.error("No date provided");
+    return formatDate(new Date());
+  }
+  if (typeof date === "string") {
+    date = new Date(date);
+  }
+  const year: string = String(date.getFullYear());
+  const month: string = String(date.getMonth() + 1).padStart(2, "0"); // months are 0 indexes i.e. jan = 0
+  const day: string = String(date.getDate()).padStart(2, "0");
+
+  const finalDate: string = `${year}-${month}-${day}`;
+  return finalDate;
+}
+// HH:MM:SS -> Date object
+export function timeStringToDate(time: string): Date {
+  // Does not validate whether time string is in the correct format
+  const hours = parseInt(time.slice(0, 2));
+  const minutes = parseInt(time.slice(3, 5));
+
+  let date: Date = new Date();
+  date.setHours(hours, minutes);
+
+  return date;
+}
+
+// Date object -> Formatted time HH:MM
+export function extractTime(inputDate: Date | string): string {
+  const date = new Date(inputDate);
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${hours}:${minutes}`;
+}
+
+export function addMinutesToDateTime(
+  dateString: string,
+  timeString: string,
+  minutes: string | number,
+): string {
+  const newDateTime: Date = new Date(`${dateString}T${timeString}`);
+  let duration: number;
+  if (typeof minutes === "string") {
+    duration = parseInt(minutes);
+  } else {
+    duration = minutes;
+  }
+
+  newDateTime.setMinutes(newDateTime.getMinutes() + duration); // Automatically rolls the hour over in case it does go over
+
+  const newHours: string = String(newDateTime.getHours()).padStart(2, "0");
+  const newMinutes: string = String(newDateTime.getMinutes()).padStart(2, "0"); // 'minutes' has already been declared??
+  return `${formatDate(newDateTime)}T${newHours}:${newMinutes}:00`;
+}
