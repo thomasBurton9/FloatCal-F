ab1886ba44ca5658016a9ea4312492d9a21accd2
Thomas Burton <thomasburton9@protonmail.com>
2026-08-13 11:14:40 +1000

    chore(backend): allowed all origins to allow for free hosting

--- backend/main.py
+++ backend/main.py
@@ -13,15 +13,15 @@ app.include_router(users.router)
 app.include_router(authentication.router)
 
 # Allow testing using react native web
-origins = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://localhost:8081",
-]  # Allow for web testing preventing CORS error converting regular requests to options requests.
+# origins = [
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+#     "http://localhost:8081",
+# ]  # Allow for web testing preventing CORS error converting regular requests to options requests.
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allow all origins for public hosting -> For hosting on vps
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
