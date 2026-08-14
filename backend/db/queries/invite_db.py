abe41cd852d81ea273ddd55ee7767df68b1c719a
Thomas Burton <thomasburton9@protonmail.com>
2026-08-14 19:55:54 +1000

    feat(backend): add params to invite db query functions

--- backend/db/queries/invite_db.py
+++ backend/db/queries/invite_db.py
@@ -1,16 +1,18 @@
 
-def invite_user_to_calendar():
+def invite_user_to_calendar(user_invite_from_id: int, user_to_invite_id, calendar_id: int) -> int:
     raise NotImplementedError
 
-def respond_to_invite():
+def respond_to_invite(user_id: int, invite_id: int, accepted: bool):
     raise NotImplementedError
     
-def get_invites_to_user():   
+def get_invites_to_user(user_id: int) -> list[Invite]:   
     raise NotImplementedError
 
 # potentially not needed by frontend
-def get_invites_from_user():
+def get_invites_from_user(user_id: int) -> list[Invite]:
     raise NotImplementedError
-    
-def get_invite_by_calendar():
+
+# user id required to make sure only the owner can request the id
+
+def get_invites_for_calendar(user_id: int, calendar_id: int) -> list[Invite]:
     raise NotImplementedError
