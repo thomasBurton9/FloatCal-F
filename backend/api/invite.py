c095b6c6b306a98b7bc44bb709e88d6d5d0a3f07
Thomas Burton <thomasburton9@protonmail.com>
2026-08-14 19:36:15 +1000

    feat: wire base of invite api endpoint

---
+++ backend/api/invite.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/api")
+
+def invite_user_to_calendar_api():
+    raise NotImplementedError
+
+def respond_to_invite_api():
+    raise NotImplementedError
+    
+def get_invites_to_user_api():   
+    raise NotImplementedError
+
+# potentiall not needed by frontend
+def get_invites_from_user_api():
+    raise NotImplementedError
+    
+def get_invite_by_calendar_api():
+    raise NotImplementedError
