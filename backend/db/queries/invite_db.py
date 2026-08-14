6215265f49528995ac25e6c434a419db57fb1162
Thomas Burton <thomasburton9@protonmail.com>
2026-08-14 19:44:12 +1000

    feat(backend): added base db query functions

---
+++ backend/db/queries/invite_db.py
@@ -0,0 +1,16 @@
+
+def invite_user_to_calendar():
+    raise NotImplementedError
+
+def respond_to_invite():
+    raise NotImplementedError
+    
+def get_invites_to_user():   
+    raise NotImplementedError
+
+# potentially not needed by frontend
+def get_invites_from_user():
+    raise NotImplementedError
+    
+def get_invite_by_calendar():
+    raise NotImplementedError
