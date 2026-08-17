1b51081dae32c97b478fa81d240bc5f70945035b
Thomas Burton <thomasburton9@protonmail.com>
2026-08-17 13:04:06 +1000

    feat(frontend): added responding to invites api function

--- frontend/src/api/inviteApi.ts
+++ frontend/src/api/inviteApi.ts
@@ -6,7 +6,7 @@ export async function inviteUser(
   calendarId: number,
 ) {
   try {
-    const inviteUserURL =
+    const inviteUserUrl =
       API_URL +
       "/invite_user/" +
       String(userId) +
@@ -15,7 +15,7 @@ export async function inviteUser(
       "/" +
       String(calendarId);
 
-    const response = await fetch(inviteUserURL, {
+    const response = await fetch(inviteUserUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -31,6 +31,40 @@ export async function inviteUser(
     return { success: true };
   } catch (error) {
     console.error("Error inviting user:", error);
+    return {
+      success: false,
+    };
+  }
+}
+
+export async function respondToInvite(
+  userId: number,
+  inviteId: number,
+  accepted: boolean,
+) {
+  try {
+    const respondToInviteUrl =
+      API_URL + "/respond_to_invite/" + String(userId) + "/" + String(inviteId);
+
+    const response = await fetch(respondToInviteUrl, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        accepted: accepted,
+      }),
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.error("Error responding to invite: ", data);
+      return { success: false, error: data.detail };
+    }
+
+    return { success: true };
+  } catch (error) {
+    console.error("Error responding to invite", error);
     return {
       success: false,
     };
