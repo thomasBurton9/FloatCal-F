3319bfce3b2041977e0b6ae49d249928f96f33a0
Thomas Burton <thomasburton9@protonmail.com>
2026-08-20 08:20:45 +1000

    feat(frontend): added listing users api function

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -21,3 +21,25 @@ export async function fetchUserInfo(userId) {
     console.error("Error fetching user info: ", error);
   }
 }
+
+export async function listUsers() {
+  try {
+    const listUsersUrl = API_URL + "/list_users";
+    const response = await fetch(listUsersUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.error("Error fetching user info", data);
+      return { success: false, error: data.detail };
+    }
+    return { success: true, result: data };
+  } catch (error) {
+    console.error("Error fetching user info: ", error);
+    return { success: false };
+  }
+}
