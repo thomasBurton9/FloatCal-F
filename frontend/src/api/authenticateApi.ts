48ffca9e5f9aa0fd83a7cd51813121583548262d
Thomas Burton <thomasburton9@protonmail.com>
2026-08-22 15:41:03 +1000

    feat(frontend): added delete account api function

---
+++ frontend/src/api/authenticateApi.ts
@@ -0,0 +1,32 @@
+import { API_URL } from "../constants";
+
+export async function deleteUser(
+  userId: number,
+  email: string,
+  password: string,
+) {
+  try {
+    const deleteUserUrl =
+      API_URL + "/authentication/" + String(userId) + "/delete_user";
+    const response = await fetch(deleteUserUrl, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.error("Error deleting user", data);
+      return { success: false, error: data.detail };
+    }
+    return { success: true };
+  } catch (error) {
+    console.error("Error deleting user: ", error);
+    return { success: false };
+  }
+}
